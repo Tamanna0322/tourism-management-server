fc8729baa948a08efba84f1c011cc16b5264c5e2
Tamanna0322 <tamanna0322@gmail.com>
2024-04-30 14:36:22 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -56,6 +56,13 @@ async function run() {
       const addTourist = req.body;
       console.log(addTourist)
       const result = await touristCollection.insertOne(addTourist);
+      res.send(result);
+    })
+
+    app.delete('/add/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await touristCollection.deleteOne(query);
       res.send(result);
     })
   
